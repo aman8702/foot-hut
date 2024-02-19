b8574e9d67851849ef2edf602d1c3ca8253d8147
unknown <amansingh05263@gmail.com>
2024-02-19 19:46:15 +0530

    new change

--- src/Components/Shoppingsec.js
+++ src/Components/Shoppingsec.js
@@ -5,40 +5,51 @@ import red from './red.jpg'
 import img1 from './1.jpg'
 import img2 from './2.jpg'
 import img3 from './3.jpg'
+import { useState } from 'react';
 
-export default function Shoppingsec() {
+export default function Shoppingsec(props) {
+    const [text,setText]=useState("Rs.14999");
+    const handelclick = ()=>{
+        console.log("Clicked");
+        setText("Price dropped Rs.13499 (10% off)")
+    }
+   
   return (
     <div>
-      <div class="container my-4">
-         <div class="row">
-             <div class="col">
+      <div className="container my-4">
+         <div className="row">
+             <div className="col">
                 <img src={prd1} height='200' width='300' alt=''/>
                 <div>
                     <p>Jordan Air 1<br/>UK 7-10<br/><br/>Rs.4999
                     <br/>
-                    <button type="button" class="btn btn-primary">Buy now</button>
+                    <button type="button" className="btn btn-primary" >Buy now</button>
                     
                     </p>
                 </div>
                     
             </div>
-            <div class="col">
+            <div className="col">
             <img src={pine} height='200' width='300' alt=''/>   
             <div>
-                    <p>Jordan Pine green Sb-4<br/>UK 7-10<br/><br/>Rs.14999
+                    <p>Jordan Pine green Sb-4<br/>UK 7-10<br/><br/>{text}
+                    
                     <br/>
-                    <button type="button" class="btn btn-primary">Buy now</button>
+                    <button type="button" className="btn btn-primary" >{props.buynow}</button>
+                    <div className='mt-2'>
+                    <button type="button" className="btn btn-success" onClick={handelclick}>Check Offers</button>
+                    </div>
                     
                     </p>
                 </div>
                 
             </div>
-            <div class="col">
+            <div className="col">
             <img src={red} height='200' width='300' alt=''/>  
             <div>
                     <p>Cherry red Canvas<br/>UK 7-10<br/><br/>Rs.5999
                     <br/>
-                    <button type="button" class="btn btn-primary">Buy now</button>
+                    <button type="button" className="btn btn-primary">Buy now</button>
                     
                     </p>
                 </div>
@@ -46,36 +57,36 @@ export default function Shoppingsec() {
              </div>
         </div>
         </div>
-        <div class="container my-4">
-         <div class="row">
-             <div class="col">
+        <div className="container my-4">
+         <div className="row">
+             <div className="col">
                 <img src={img1} height='200' width='300' alt=''/>
                 <div>
                     <p>Chunky yellow m1-2<br/>UK 7-10<br/><br/>Rs.7999
                     <br/>
-                    <button type="button" class="btn btn-primary">Buy now</button>
+                    <button type="button" className="btn btn-primary">Buy now</button>
                     
                     </p>
                 </div>
                     
             </div>
-            <div class="col">
+            <div className="col">
             <img src={img2} height='200' width='300' alt=''/>   
             <div>
                     <p>Jordan Air 1<br/>UK 7-10<br/><br/>Rs.3999
                     <br/>
-                    <button type="button" class="btn btn-primary">Buy now</button>
+                    <button type="button" className="btn btn-primary">Buy now</button>
                     
                     </p>
                 </div>
                 
             </div>
-            <div class="col">
+            <div className="col">
             <img src={img3} height='200' width='300' alt=''/>  
             <div>
                     <p>Notion brown sneaker<br/>UK 7-10<br/><br/>Rs.8999
                     <br/>
-                    <button type="button" class="btn btn-primary">Buy now</button>
+                    <button type="button" className="btn btn-primary">Buy now</button>
                     
                     </p>
                 </div>
